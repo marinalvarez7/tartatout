8c1d698d02b60f02a392af27dd0eb78202938a6d
eblancardi <emmanuelle.blancardi@gmail.com>
2020-04-07 19:54:28 +0200

    heroku bugs

--- bin/seeds.js
+++ bin/seeds.js
@@ -11,7 +11,9 @@ const mongoose = require("mongoose");
 const json = require("./data.json");
 const Recipe = require("../models/Recipe");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/tartatout", { useNewUrlParser: true });
+const app_name = require('./package.json').name; // "tartatout"
+
+mongoose.connect((process.env.MONGODB_URI || `mongodb://localhost/${app_name}`), { useNewUrlParser: true });
 
 //mongoose.connection.db.dropDatabase();
 
